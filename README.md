25eca8c95755ed1fdde414a01253c2fbc01067c3
Andrew Root <root.drew27@gmail.com>
2023-05-18 14:57:32 -0500

    season input functionality added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 This is the beginning of an application, which will be used to predict the outcomes of sporting events
 
+
+
+API CALLS TESTING FILE
+    
+	This script should utilize the free-nba api to determine what games in a particular season, had the highest combined score
+	(Note: on season repr, the 2018-2019 season is repr as 2018)
--- api_calls_testing.py
+++ api_calls_testing.py
@@ -1,21 +1,37 @@
 import requests
 import json
 
-url = "https://free-nba.p.rapidapi.com/games"
 
-querystring = {"page":"0","per_page":"25"}
+if __name__ == "__main__":
+    
 
-headers = {
-	"X-RapidAPI-Key": "d8b5e4c393mshdeeb57ca5e71e4dp1a1915jsn2f371c4f6d98",
-	"X-RapidAPI-Host": "free-nba.p.rapidapi.com"
-}
+	print("This script will give you the highest combined score of any season(s) from the NBA. It will return the Season, the Teams (the game), the date, the individual scores, and the combined score")
 
-response = requests.get(url, headers=headers, params=querystring)
+	#if the wrong form/type of input is entered, the script will fail
+	seasons = input("Enter the season(s) as a comma seperated list (eg. 2018, 2020, 2021): ")
+	
 
-
-json_obj = response.json()
-json_str = json.dumps(json_obj)
+	url = "https://free-nba.p.rapidapi.com/games"
 
 
-with open("nba_games.json", "w") as write_file:
-    json.dump(json_obj, write_file)
+	querystring = {"page":"0","per_page":"25", "Seasons":f"{seasons}"}
+
+	headers = {
+		"X-RapidAPI-Key": "d8b5e4c393mshdeeb57ca5e71e4dp1a1915jsn2f371c4f6d98",
+		"X-RapidAPI-Host": "free-nba.p.rapidapi.com"
+	}
+
+	response = requests.get(url, headers=headers, params=querystring)
+
+
+	json_obj = response.json()
+	json_str = json.dumps(json_obj, sort_keys=True, indent=4)
+
+	print(json_str)
+
+
+	# with open("nba_games.json", "w") as write_file:
+	# 	json.dump(json_obj, write_file)
+
+
+
